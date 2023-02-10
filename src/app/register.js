6240d875728ab0cd033156bf9425a9f2278b605e
thatOneJon <83345498+ThatOneJon@users.noreply.github.com>
2023-02-10 22:26:23 +0100

    register

--- src/app/register.js
+++ src/app/register.js
@@ -6,10 +6,10 @@ export default function Register(){
 
     const[registerIn, setRegisterIn] = React.useState({
         username:"",
-        mail: "",
+        email: "",
         password: ""
     })
-
+    const[responseErr,setResponseErr] = React.useState(null)
 
 
    function handleChange(event){
@@ -19,29 +19,47 @@ export default function Register(){
         }else if(ident ==="pass"){
             setRegisterIn(p => ({...p, password: event.target.value}))
         }else{
-            setRegisterIn(p => ({...p, mail: event.target.value}))
+            setRegisterIn(p => ({...p, email: event.target.value}))
         }
     }
 
     async function handleSubmit(){
-            await fetch("https://immpapi.onrender.com/api/register", {
-            method: "POST",
-            body: JSON.stringify(registerIn)
+            setRegisterIn({
+                username:"",
+                email: "",
+                password: ""
             })
-            .then(response => console.log(response.json()))
-            .then(data => console.log(data))
+            await fetch("https://immpapi.onrender.com/api/register", {
+            method: "post",
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+           },
+                body: JSON.stringify(registerIn)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                data.err ? (setResponseErr(data.err)) : setResponseErr("Success! Welcome" + data.username)
+            })
         }
 
 
     return(
+        <>
         <div className="mr-2 mt-1 flex flex-col lg:flex-row align-center border-l-2 px-1">
+            { responseErr && <div onClick={() => setResponseErr(null)} className="bg-white absolute text-black p-3 ml-7 mt-10 border-2 border-red-900 rounded">
+                <p>Error: {responseErr} </p>
+                <button onClick={() => setResponseErr(null)} className="bg-slate-800 px-3 py-1 text-white rounded">Ok</button>
+            </div>}
             <label className=" mx-1">Username: </label>
             <input onChange={(event) => handleChange(event)} value= {registerIn.username} name="name" type="text" className="text-black w-full md:w-2/3 rounded mx-1 my-1 px-3 focus:lg:absolute focus:lg:mt-12 focus:lg:w-44"/>
             <label className=" mx-1">Email: </label>
-            <input onChange={(event) => handleChange(event)} value= {registerIn.mail} name="mail" type="text" className="text-black w-full md:w-2/3 rounded mx-1 my-1 px-3 focus:lg:absolute focus:lg:mt-12 focus:lg:w-44"/>
+            <input onChange={(event) => handleChange(event)} value= {registerIn.email} name="mail" type="text" className="text-black w-full md:w-2/3 rounded mx-1 my-1 px-3 focus:lg:absolute focus:lg:mt-12 focus:lg:w-44"/>
             <label className=" mx-1">Password: </label>
-            <input onChange={(event) => handleChange(event)} value= {registerIn.password} name="pass" type="text" className="text-black w-full md:w-2/3 rounded mx-1 my-1 px-3 focus:lg:absolute focus:lg:mt-12 focus:lg:w-44"/>
+            <input onChange={(event) => handleChange(event)} value= {registerIn.password} name="pass" type="password" className="text-black w-full md:w-2/3 rounded mx-1 my-1 px-3 focus:lg:absolute focus:lg:mt-12 focus:lg:w-44"/>
             <button onClick ={handleSubmit} className="py-1 px-2 ml-1 bg-gray-500 rounded shadow-xl hover:bg-gray-600">Go</button>
         </div>
+        </>
     )
 }
